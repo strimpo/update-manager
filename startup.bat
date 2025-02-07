6b4b6aee9c43d5002c11149d27b0084096856996
Noah Hay <eepyboat@gmail.com>
2025-02-06 18:16:20 -0600

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -9,5 +9,5 @@ pause
 goto checkfile
 
 :checkfile
-set filecheck=\gui\menu.bat \cmd\startup\check\req\os\doc.txt \cmd\startup\check\req\os\wifisetup.xml \cmd\startup\check\req\external\cd.txt \cmd\startup\check\req\internal\motherboard.txt \cmd\startup\check\req\internal\ram
+set filecheck=\gui\menu.bat \cmd\startup\check\req\os\doc.txt \cmd\startup\check\req\os\wifisetup.xml \cmd\startup\check\req\external\cd.txt \cmd\startup\check\req\external\port.txt \cmd\startup\check\req\internal\mb.txt \cmd\startup\check\req\internal\ram.txt \cmd\startup\check\req\external\usb\nmb\usb1.txt \cmd\startup\check\req\external\usb\nmb\usb2.txt \cmd\startup\check\req\external\usb\nmb\usb3.txt \cmd\startup\check\req\external\usb\nmb\usb4.txt \cmd\startup\check\req\external\usb\nmb\usb5.txt \cmd\startup\check\req\external\usb\nmb\usb6.txt \cmd\startup\check\req\external\usb\nmb\usb7.txt
 
